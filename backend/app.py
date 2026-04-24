e0007a3f1e6d9e90c072f05e564339b150e3203e
Christoffer Lilja <christoffer.lilja@zilux.se>
2026-04-24 09:55:25 +0000

    Update hero text and bind to IPv6

--- backend/app.py
+++ backend/app.py
@@ -75,4 +75,4 @@ def create_app():
 if __name__ == '__main__':
     app = create_app()
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='::', port=port, debug=True)
